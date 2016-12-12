4758a8ed3e18cbed3dc580a928d354fbfd5318da
CHARMEDALLOY\avalj <avaljotsingh@gmail.com>
2016-12-12 16:40:39 -0600

    Changed location to austin

--- src/main/scala/Application/MyApp.scala
+++ src/main/scala/Application/MyApp.scala
@@ -12,7 +12,7 @@ import org.json4s.JsonDSL.WithDouble._
 object MyApp {
 
   def main(args: Array[String]): Unit = {
-    val Point_Of_Interest = "India"
+    val Point_Of_Interest = "Austin"   //User location ( we can get this from browser when implementing web-app)
     val maxDistance = 1.0
     val todayDate = new DateTime()
     val nextDate = todayDate.plusDays(1)
